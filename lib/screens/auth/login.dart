e4aadc89299dee2b04d0ba93182591968bca5f0c
AasharWahla <AasharWahla@users.noreply.github.com>
2021-01-29 01:02:15 +0500

    V 0.0.3
    Update the Login page.
    Added the email and password field.
    Added the show password.
    Added custom color to the label text.

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -10,6 +10,38 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   double height;
   double width;
+  FocusNode _emailFocusNode;
+  FocusNode _passwordFocusNode;
+  bool _showPassword = false;
+  String email;
+  String password;
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode = FocusNode();
+    _passwordFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _requestEmailFocus() {
+    setState(() {
+      FocusScope.of(context).requestFocus(_emailFocusNode);
+    });
+  }
+
+  void _requestPasswordFocus() {
+    setState(() {
+      FocusScope.of(context).requestFocus(_passwordFocusNode);
+    });
+  }
+
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
@@ -27,14 +59,14 @@ class _LoginScreenState extends State<LoginScreen> {
             Container(
               height: height * .1,
               width: width,
-              padding: EdgeInsets.only(right: width * .1),
+              padding: EdgeInsets.only(right: width * .15),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   SizedBox(width: 20.0, height: 100.0),
                   RotateAnimatedTextKit(
                     repeatForever: true,
-                    text: ["BREAK", "MAKE", "TRACK", "IMPROVE"],
+                    text: ["BREAK", "MAKE", "TRACK"],
                     textStyle: GoogleFonts.varelaRound(
                       fontSize: 40,
                       fontWeight: FontWeight.bold,
@@ -52,7 +84,115 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             // Simple Text
+            Container(
+              height: height * .1,
+              width: width * .9,
+              child: Center(
+                child: FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    "Login to Start your Journey!",
+                    style: GoogleFonts.varelaRound(
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ),
             // Login form
+            Container(
+              width: width * .9,
+              height: height * .3,
+              child: Center(
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(),
+                        child: TextFormField(
+                          focusNode: _emailFocusNode,
+                          onTap: _requestEmailFocus,
+                          style: GoogleFonts.varelaRound(),
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.email,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Colors.amber,
+                              ),
+                            ),
+                            labelText: (_emailFocusNode.hasFocus)
+                                ? "e-mail"
+                                : "\te-mail",
+                            labelStyle: GoogleFonts.varelaRound(
+                              color: (_emailFocusNode.hasFocus)
+                                  ? Colors.red
+                                  : Colors.grey,
+                              fontSize: 25,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(),
+                        child: TextFormField(
+                          obscureText: (_showPassword) ? false : true,
+                          focusNode: _passwordFocusNode,
+                          onTap: _requestPasswordFocus,
+                          style: GoogleFonts.varelaRound(),
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.vpn_key,
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                                color: Colors.amber,
+                              ),
+                            ),
+                            suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(
+                                  () {
+                                    _showPassword = !_showPassword;
+                                  },
+                                );
+                              },
+                              child: Icon((_showPassword)
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                            ),
+                            labelText: (_passwordFocusNode.hasFocus)
+                                ? "password"
+                                : "\tpassword",
+                            labelStyle: GoogleFonts.varelaRound(
+                              color: (_passwordFocusNode.hasFocus)
+                                  ? Colors.red
+                                  : Colors.grey,
+                              fontSize: 25,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             // forget password
             //login
             // space
